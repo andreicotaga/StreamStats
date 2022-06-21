7b116dd9be3ecb71e9100ec90396caf6856d0e49
Andrei Cotaga <andrei.cotaga@menu.app>
2022-06-21 12:13:42 +0300

    Add necessary routes and resources

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\TwitchOauthCallbackController;
+use App\Http\Controllers\TwitchOauthRedirectController;
+use App\Http\Livewire\Dashboard;
+use App\Http\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', Welcome::class)->name('landing');
+Route::get('/auth/twitch/redirect', TwitchOauthRedirectController::class)->name('oauth.twitch.redirect');
+Route::get('/auth/twitch/callback', TwitchOauthCallbackController::class)->name('oauth.twitch.callback');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::post('/logout', LogoutController::class)->name('logout');
 });
