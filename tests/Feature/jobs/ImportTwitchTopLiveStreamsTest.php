a851c43aca8ec92865c63c5574b8b27b1797a09c
Andrei Cotaga <andrei.cotaga@menu.app>
2022-06-21 11:49:56 +0300

    Add functional tests for existing features

---
+++ tests/Feature/jobs/ImportTwitchTopLiveStreamsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\jobs;
+
+use App\Jobs\ImportTwitchTopLiveStreamsJob;
+use App\Models\Game;
+use App\Models\Stream;
+use App\Models\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ImportTwitchTopLiveStreamsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_should_import_live_streams()
+    {
+        // Can't test it in workflow without sharing sensitive data
+        if (!config('twitch-api.client_id')) {
+            $this->assertTrue(true);
+            return;
+        }
+
+        $job = new ImportTwitchTopLiveStreamsJob(50);
+        $job->handle();
+
+        $this->assertTrue(Stream::count() > 40);
+        $this->assertTrue(Game::count() > 0);
+        $this->assertTrue(Tag::count() > 0);
+    }
+}
