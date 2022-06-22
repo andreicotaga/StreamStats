eccbb4b23f91a9a01af1640fc5ca757c7959911a
Andrei Cotaga <andrei.cotaga@menu.app>
2022-06-22 15:19:38 +0300

    first laravel deployment to heroku

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', Welcome::class)->name('landing');
 Route::get('/auth/twitch/redirect', TwitchOauthRedirectController::class)->name('oauth.twitch.redirect');
 Route::get('/auth/twitch/callback', TwitchOauthCallbackController::class)->name('oauth.twitch.callback');
 
-//Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
     Route::post('/logout', LogoutController::class)->name('logout');
-//});
+});
