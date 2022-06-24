4f74dcf5f901c924782de9dba92c945dee9051ff
Andrei Cotaga <andrei.cotaga@menu.app>
2022-06-24 16:09:13 +0300

    Catch twitch exception in case email is not provided.

--- app/Http/Controllers/TwitchOauthCallbackController.php
+++ app/Http/Controllers/TwitchOauthCallbackController.php
@@ -3,32 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
+use Exception;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class TwitchOauthCallbackController extends Controller
 {
+    /**
+     * @throws Exception
+     */
     public function __invoke()
     {
         $twitchUser = Socialite::driver('twitch')->user();
 
         $user = User::where('twitch_id', $twitchUser->id)->first();
 
-        if (!$user) {
-            $user = User::create([
-                'twitch_id'    => $twitchUser->id,
-                'twitch_token' => $twitchUser->token,
-                'name'         => $twitchUser->name,
-                'email'        => $twitchUser->email,
-                'password'     => Str::random(),
-            ]);
-        } else {
-            $user->update([
-                'twitch_token' => $twitchUser->token,
-                'name'         => $twitchUser->name,
-                'email'        => $twitchUser->email,
-            ]);
+        try {
+            if (!$user) {
+                $user = User::create([
+                    'twitch_id'    => $twitchUser->id,
+                    'twitch_token' => $twitchUser->token,
+                    'name'         => $twitchUser->name,
+                    'email'        => $twitchUser->email,
+                    'password'     => Str::random(),
+                ]);
+            } else {
+                $user->update([
+                    'twitch_token' => $twitchUser->token,
+                    'name'         => $twitchUser->name,
+                    'email'        => $twitchUser->email,
+                ]);
+            }
+        } catch (QueryException $e) {
+            throw new Exception('Please verify your twitch account email address.', $e->getMessage());
         }
 
         auth()->login($user);
